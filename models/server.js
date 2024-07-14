c20a5d837de743beaf7cf585a8364e4dc6ebeb86
Anne <annaconda1984@yahoo.co.uk>
2024-07-14 01:22:53 +0100

    Filmflixupdate

--- models/server.js
+++ models/server.js
@@ -4,10 +4,6 @@ const express = require('express');
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const session = require('express-session');
-const MongoStore = require("connect-mongo");
-
-
 
 // Controllers / Routes
 const authController = require("./controllers/auth.js");
@@ -26,36 +22,17 @@ app.use(methodOverride('_method'))
 // Morgan for logging HTTP requests
 app.use(morgan('dev'))
 app.set('view engine', 'ejs')
-app.use(
-    session({
-      secret: process.env.SESSION_SECRET,// the secret is used to encrypt and decript the data n the session
-      resave: false,// we do not need to force a save of an unmodified session
-      saveUninitialized: true,// enforce a save of a new session that has yet to be initialised
-      store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI,
-    }),
-  })
-)
-
 
 // Routes
-
-
-// Landing page
 app.get('/', async (req, res) => {
-    
-    res.render('index', {
-        user: req.session.user,
-    });
+    res.render('index');
   });
-
-
-  // Auth
+  
   app.use("/auth", authController);
 
 
 
-// Connections
+// Server connections
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
